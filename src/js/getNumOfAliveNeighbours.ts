92e9826e7b733d2c7163326324cc7033fc8aa3cc
oldbelom <oldbelom@gmail.com>
2021-11-10 15:47:08 +0300

    add getNumOfAliveNeighbours function and tests

---
+++ src/js/getNumOfAliveNeighbours.ts
@@ -0,0 +1,22 @@
+import { getCellState } from "./getCellState";
+
+export function getNumOfAliveNeighbours(
+  column: number,
+  row: number,
+  field: number[][]
+): number {
+  let neighbours = 0;
+
+  for (let j = column - 1; j <= column + 1; j += 1) {
+    neighbours += Number(getCellState(field, j, row - 1));
+  }
+
+  for (let j = column - 1; j <= column + 1; j += 1) {
+    neighbours += Number(getCellState(field, j, row + 1));
+  }
+
+  neighbours += Number(getCellState(field, column - 1, row));
+  neighbours += Number(getCellState(field, column + 1, row));
+
+  return neighbours;
+}
